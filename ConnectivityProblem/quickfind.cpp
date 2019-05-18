b59e72c5ea4bf52e76bd32a25b3271919f120490
Ritesh Singh Chauhan <riteshc@qti.qualcomm.com>
2019-05-18 22:06:02 +0530

    quick find

--- ConnectivityProblem/quickfind.cpp
+++ ConnectivityProblem/quickfind.cpp
@@ -6,16 +6,18 @@ int main(){
     int numberOfVertices;
     cin >> numberOfVertices;
     int ids[numberOfVertices];
+    for(int i = 0; i< numberOfVertices; i++)
+        ids[i] = i;
     int p, q;
     int numberOfPairs;
-    cin >> numberofPairs;
+    cin >> numberOfPairs;
     for(int i = 0; i< numberOfPairs; i++){
         cin >> p >> q;
         if(ids[p]==ids[q])
             continue;
-        for(int j = 0; j< numberOfVertices; j++)[
-            if(ids[i] == p)
-                ids[i] = ids[q];
+        for(int j = 0; j< numberOfVertices; j++)
+            if(ids[j] == p)
+                ids[j] = ids[q];
         cout << p << " " << q << endl;
     }
 }
